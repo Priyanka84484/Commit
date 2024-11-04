3b0269760fc6a9695bc8f213d67ffa597479ed1a
Priyanka <thakur84484@gmail.com>
2024-11-04 17:00:38 +0530

    this practice

--- Assignment1/src/Assignment6.java
+++ Assignment1/src/Assignment6.java
@@ -3,7 +3,7 @@ public class Assignment6 {
 	
 	public void m0() {
 		System.out.println("Default Method");
-		this.m4();
+		//this.m4();
 	}
 
 	public void m1() {
@@ -16,10 +16,12 @@ public class Assignment6 {
 	public void m3() {
 		System.out.println("3 Parameterized Method");
 		this.m0();
+		this.m4();
+this.m2();
 	}
 	public void m4() {
 		System.out.println("4 Parameterized Method");
-		this.m2();
+		//this.m2();
 	}
 	
 	public static void main(String[] args) {
