b3d7c62fe065402563fe9f8eab7755d483b40d12
Priyanka <thakur84484@gmail.com>
2024-11-04 18:28:43 +0530

    has a relationship assignment

---
+++ Assignment1/src/has_a_relationship.java
@@ -0,0 +1,12 @@
+
+public class has_a_relationship {
+
+	public static void main(String[] args) {
+		
+		Assignment6 obj = new Assignment6();
+		obj.m0();
+		
+		Assign5 cons = new Assign5();
+	}
+}
+		
